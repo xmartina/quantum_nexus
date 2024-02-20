f3a05a7ea32d9a96ff9e984280903a9d9f4ef9b9
xmartina <martinsprince126@gmail.com>
2024-02-20 14:13:12 +0100

    d

--- user_panel/dashboard.php
+++ user_panel/dashboard.php
@@ -1,7 +1,7 @@
 <?php
 const rootDir = '/home/multistream6/domains/quantumnexusai.online/public_html/user_panel/';
 const siteName = 'Quantum Nexus Ai';
-const siteUrl = 'https://quantumnexusai.online/';
+const siteUrl = 'https://quantumnexusai.online/front/';
 include_once (rootDir.'header.php');
 ?>
 <div class="page-content-wrapper">
